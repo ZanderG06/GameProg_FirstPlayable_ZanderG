60d9f82b70326041a236a3ac5f0b5edba64e4d50
Zander Goodwin <zanderngoodwin@gmail.com>
2025-11-17 10:16:33 -0400

    Initial commit

---
+++ GameProg_FirstPlayable_ZanderG/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProg_FirstPlayable_ZanderG
+{
+    internal class Program
+    {
+        static string map = "map.txt";
+        
+        static void Main(string[] args)
+        {
+            DisplayMap();
+        }
+
+        static void DisplayMap()
+        {
+            for (int i = 0; i < map.Length + 2; i++) Console.Write("░");
+
+            Console.Write("\n");
+
+
+        }
+    }
+}
